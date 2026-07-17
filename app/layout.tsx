b7aa433c7d779b52884f4ff25007db47ddfeeff2
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-07-17 21:11:29 +0600

    28-7 Integrating ShadcnU

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+import "./globals.css";
+import { Raleway } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const raleway = Raleway({subsets:['latin'],variable:'--font-sans'});
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className={cn("h-full antialiased", "font-sans", raleway.variable)}>
       <body className="min-h-full flex flex-col">
         {children}
       </body>
