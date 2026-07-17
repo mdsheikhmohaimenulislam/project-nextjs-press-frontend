e8968d9812178ac771199f90b59f054a14b2ab90
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-07-17 22:35:10 +0600

    28-7 Integrating ShadcnU

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -12,6 +12,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 
+
 export default function LoginPage() {
   return (
     <Card className="w-full max-w-sm">
