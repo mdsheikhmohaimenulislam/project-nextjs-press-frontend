070d46a3021c4bada8c73596f80109039e233db8
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-07-24 19:43:34 +0600

    31-3 Image Component In NextJS

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images : {
+    remotePatterns:[
+      {
+        hostname: "static.vecteezy.com"
+      },
+      {
+        hostname: "example.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
