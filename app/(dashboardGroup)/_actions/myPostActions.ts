785f16c1ee655b33695304a3c81b7e10aba82fb7
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-07-25 10:37:59 +0600

    32-1 Implementing Functionalities Of My Posts Page

---
+++ app/(dashboardGroup)/_actions/myPostActions.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const getMyPosts = async () => {
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+
+  if (!accessToken) {
+    return {
+      success: false,
+      message: "User not logged in!",
+    };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts/my-posts`, {
+    headers: {
+      cookie: `accessToken=${accessToken}`,
+    },
+    // cache: "force-cache",
+    // next: {
+    //   revalidate: 60 * 60 * 24,
+    //   tags: ["my-post"],
+    // },
+  });
+  const result = res.json();
+  return result;
+};
